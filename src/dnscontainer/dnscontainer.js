ef7a6f76f7643552268133d3fab56296154b9534
lgdelacruz <bboyjacks@gmail.com>
2019-11-15 10:16:11 -0800

    Slow un optimized way

--- src/dnscontainer/dnscontainer.js
+++ src/dnscontainer/dnscontainer.js
@@ -24,28 +24,30 @@ const DNSContainer = props => {
   const page = { x: 50, y: 50, w: 500, h: 500, u: 20 };
   const [x, y] = useGrid({ w: page.w, h: page.h, u: page.u });
   const classes = useStyles({ page });
-
-  let imageDatas = [];
-  for (let i = 0; i < 2; i++) {
-    const imageData = {
-      x: 20 + 10 * i,
-      y: 100,
-      translateX: 0,
-      translateY: 0,
-      scaledWidth: 100,
-      scaledHeight: 100,
-      width: 100,
-      height: 100,
-      src: "https://source.unsplash.com/random/1000x1000",
-      alt: "random",
-      id: `unique-${i}`,
-      index: 1
-    };
-    imageDatas.push(imageData);
-  }
+  const [imageDatas, setImageDatas] = React.useState(() => {
+    let result = [];
+    for (let i = 0; i < 2; i++) {
+      const imageData = {
+        x: 20 + 10 * i,
+        y: 100,
+        translateX: 0,
+        translateY: 0,
+        scaledWidth: 100,
+        scaledHeight: 100,
+        width: 100,
+        height: 100,
+        src: "https://source.unsplash.com/random/1000x1000",
+        alt: "random",
+        id: `unique-${i}`,
+        index: 1
+      };
+      result.push(imageData);
+    }
+    return result;
+  });
 
   const containerRef = React.useRef();
-  const refs = React.useRef([React.createRef(), React.createRef()]);
+  const imageRefs = React.useRef([React.createRef(), React.createRef()]);
 
   return (
     <div ref={containerRef} className={classes.container}>
@@ -56,7 +58,19 @@ const DNSContainer = props => {
         <HorizontalLine y={yi} w={page.w} key={i} />
       ))}
       {imageDatas.map((data, i) => (
-        <Image data={data} containerRef={containerRef} />
+        <React.Fragment>
+          <Rectangle onMouseMove />
+          <Image
+            data={data}
+            containerRef={containerRef}
+            ref={imageRefs.current[i]}
+            onUpdate={() => {
+              imageDatas[i] = imageRefs.current[i];
+              setImageDatas(imageDatas);
+              console.log("New image Data", imageDatas[i]);
+            }}
+          />
+        </React.Fragment>
       ))}
     </div>
   );
