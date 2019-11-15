bcf13c0e05bf04b4642397343e848945cf40aa9c
lgdelacruz <bboyjacks@gmail.com>
2019-11-15 06:40:50 -0800

    Added image DNS

--- src/dnscontainer/dnscontainer.js
+++ src/dnscontainer/dnscontainer.js
@@ -4,6 +4,7 @@ import { useGrid } from "./gridcreator";
 import { translate } from "./translate";
 import VerticalLine from "./verticalline";
 import HorizontalLine from "./horizontalline";
+import Image from "image-drag-and-scale";
 
 const useStyles = MaterialUI.makeStyles(theme => {
   return {
@@ -21,6 +22,21 @@ const DNSContainer = props => {
   const page = { x: 50, y: 50, w: 500, h: 500, u: 20 };
   const [x, y] = useGrid({ w: page.w, h: page.h, u: page.u });
   const classes = useStyles({ page });
+
+  const imageData = {
+    x: 400,
+    y: 200,
+    translateX: 0,
+    translateY: 0,
+    scaledWidth: 100,
+    scaledHeight: 100,
+    width: 100,
+    height: 100,
+    src: "https://source.unsplash.com/random/1000x1000",
+    alt: "random",
+    id: "124",
+    index: 2
+  };
   return (
     <div className={classes.container}>
       {x.map(xi => (
@@ -29,6 +45,7 @@ const DNSContainer = props => {
       {y.map(yi => (
         <HorizontalLine y={yi} w={page.w} />
       ))}
+      <Image data={imageData} />
     </div>
   );
 };
