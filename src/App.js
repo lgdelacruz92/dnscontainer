ff141e51210a99ee1a588b62ba244f2f3217bbbe
lgdelacruz <bboyjacks@gmail.com>
2019-11-21 15:01:32 -0800

    Deleted a bunch

--- src/App.js
+++ src/App.js
@@ -3,61 +3,9 @@ import "./App.css";
 import DNSContainer from "./dnscontainer";
 
 function App() {
-  const imageData1 = {
-    x: 20,
-    y: 100,
-    translateX: 0,
-    translateY: 0,
-    scaledWidth: 100,
-    scaledHeight: 100,
-    width: 100,
-    height: 100,
-    src: "https://source.unsplash.com/random/1000x1000",
-    alt: "random",
-    id: `unique-123`,
-    index: 1,
-    type: "image"
-  };
-
-  const imageData2 = {
-    x: 20,
-    y: 100,
-    translateX: 0,
-    translateY: 0,
-    scaledWidth: 100,
-    scaledHeight: 100,
-    width: 100,
-    height: 100,
-    src: "https://source.unsplash.com/random/1000x1000",
-    alt: "random",
-    id: `unique-124`,
-    index: 2,
-    type: "image"
-  };
-
-  const textData1 = {
-    id: "id-unq-121",
-    x: 100,
-    y: 100,
-    fontWeight: "bold",
-    text: "Hello",
-    index: 0,
-    type: "text"
-  };
-
-  const datas = [textData1, imageData1, imageData2];
-  const dnsRef = React.useRef();
-
   return (
     <div className="App">
-      <DNSContainer width={500} height={700} datas={datas} ref={dnsRef} />
-      <button
-        onClick={() => {
-          console.log("Dns snapshot", dnsRef.current);
-        }}
-      >
-        Take Snapshot
-      </button>
+      <DNSContainer width={500} height={700}></DNSContainer>
     </div>
   );
 }
--- src/dnscontainer/dnscontainer.js
+++ src/dnscontainer/dnscontainer.js
@@ -1,14 +1,11 @@
 import React from "react";
 import * as MaterialUI from "@material-ui/core";
-import VerticalLine from "./grid/verticalline";
-import HorizontalLine from "./grid/horizontalline";
-import Contents from "./content/contents";
 
 const useStyles = MaterialUI.makeStyles(theme => {
   return {
     container: {
-      width: props => props.page.w,
-      height: props => props.page.h,
+      width: props => props.width,
+      height: props => props.height,
       position: "relative",
       overflow: "hidden",
       background: "white",
@@ -18,68 +15,17 @@ const useStyles = MaterialUI.makeStyles(theme => {
 });
 
 const DNSContainer = React.forwardRef((props, ref) => {
-  const { width, height, datas } = props;
-  if (!width || !height || !datas) {
+  const { width, height } = props;
+  if (!width || !height) {
     throw Error("Width, height, and imageDatas array is required");
   }
-  if (!Array.isArray(datas)) {
-    throw Error(
-      "datas must be an array. Please refer to getting started in the packages."
-    );
-  }
 
-  const page = { w: width, h: height };
-  const classes = useStyles({ page });
   const containerRef = React.useRef();
-  const vlRef = React.useRef();
-  const leftLineRef = React.useRef();
-  const rightLineRef = React.useRef();
-  const hlRef = React.useRef();
-  const topLineRef = React.useRef();
-  const bottomLineRef = React.useRef();
-
-  React.useEffect(() => {
-    const clearAllLines = () => {
-      vlRef.current.setAttribute("style", "");
-      leftLineRef.current.setAttribute("style", "");
-      rightLineRef.current.setAttribute("style", "");
-      hlRef.current.setAttribute("style", "");
-      topLineRef.current.setAttribute("style", "");
-      bottomLineRef.current.setAttribute("style", "");
-    };
-    document.addEventListener("mouseup", clearAllLines);
-    return () => {
-      document.removeEventListener("mouseup", clearAllLines);
-    };
-  }, []);
-
-  const contentsRef = React.useRef();
-
-  React.useEffect(() => {
-    if (ref) {
-      ref.current = contentsRef.current;
-    }
-  });
+  const classes = useStyles(props);
 
   return (
     <div ref={containerRef} className={classes.container}>
-      <Contents
-        ref={contentsRef}
-        vlRef={vlRef}
-        hlRef={hlRef}
-        leftLineRef={leftLineRef}
-        rightLineRef={rightLineRef}
-        topLineRef={topLineRef}
-        bottomLineRef={bottomLineRef}
-        containerRef={containerRef}
-        datas={datas}
-      />
-      <VerticalLine ref={vlRef} x={0} h={page.h} />
-      <VerticalLine ref={leftLineRef} x={0} h={page.h} />
-      <VerticalLine ref={rightLineRef} x={0} h={page.h} />
-      <HorizontalLine ref={hlRef} x={0} w={page.w} />
-      <HorizontalLine ref={topLineRef} x={0} w={page.w} />
-      <HorizontalLine ref={bottomLineRef} x={0} w={page.w} />
+      {props.children}
     </div>
   );
 });
