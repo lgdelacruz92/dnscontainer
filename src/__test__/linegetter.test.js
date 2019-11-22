c6fa962b708cb130b96b8e4438fbce84a5479968
lgdelacruz <bboyjacks@gmail.com>
2019-11-22 08:42:56 -0800

    Horizontal center complete

--- src/__test__/linegetter.test.js
+++ src/__test__/linegetter.test.js
@@ -1,7 +1,15 @@
-import { getVerticalCenter } from "../dnscontainer/utils/linegetter";
+import {
+  getVerticalCenter,
+  getHorizontalCenter
+} from "../dnscontainer/utils/linegetter";
 describe("Line getter test", () => {
   test("Get vertical center", () => {
     const result = getVerticalCenter({ y: 50, h: 100 });
+    expect(result).toBe(100);
+  });
+
+  test("Get horizontal center", () => {
+    const result = getHorizontalCenter({ x: 50, w: 100 });
     expect(result).toBe(100);
   });
 });
--- src/dnscontainer/utils/linegetter.js
+++ src/dnscontainer/utils/linegetter.js
@@ -1,3 +1,7 @@
 export const getVerticalCenter = rect => {
   return (rect.y * 2 + rect.h) / 2;
 };
+
+export const getHorizontalCenter = rect => {
+  return (rect.x * 2 + rect.w) / 2;
+};
