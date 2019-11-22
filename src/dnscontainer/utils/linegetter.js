e0a4c599ad5d5be82a9638e06bdb3a3acc6b9eaa
lgdelacruz <bboyjacks@gmail.com>
2019-11-22 08:44:37 -0800

    Completed rect getters

--- src/dnscontainer/utils/linegetter.js
+++ src/dnscontainer/utils/linegetter.js
@@ -5,3 +5,19 @@ export const getVerticalCenter = rect => {
 export const getHorizontalCenter = rect => {
   return (rect.x * 2 + rect.w) / 2;
 };
+
+export const getLeft = rect => {
+  return rect.x;
+};
+
+export const getTop = rect => {
+  return rect.y;
+};
+
+export const getRight = rect => {
+  return rect.x + rect.w;
+};
+
+export const getBottom = rect => {
+  return rect.y + rect.h;
+};
