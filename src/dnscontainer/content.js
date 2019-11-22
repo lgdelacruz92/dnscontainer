dabf271a03803431809ef6e62c3f24bf2773e242
lgdelacruz <bboyjacks@gmail.com>
2019-11-21 16:44:24 -0800

    Added content

---
+++ src/dnscontainer/content.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Image from "./image/image";
+import Text from "./text/text";
+
+const Content = props => {
+  const { containerRef, data, onChange, onChangeEnd } = props;
+  if (data.type === "image") {
+    return (
+      <Image
+        key={data.id}
+        containerRef={containerRef}
+        id={data.id}
+        onChange={onChange}
+        onChangeEnd={onChangeEnd}
+      />
+    );
+  } else if (data.type === "text") {
+    return <Text key={data.id} id={data.id} onChange={onChange} />;
+  } else {
+    return null;
+  }
+};
+
+export default Content;
