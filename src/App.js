fa4304f30b6aad47b666fd32d7193a4c3af1001b
lgdelacruz <bboyjacks@gmail.com>
2019-11-26 06:19:44 -0800

    Listing to undo trigger

--- src/App.js
+++ src/App.js
@@ -4,13 +4,35 @@ import DNSContainer, { DNSImage } from "./dnscontainer";
 import { imageData } from "./dnscontainer/data";
 
 function App() {
+  const [data, setImageData] = React.useState(imageData);
+  const [history, setHistory] = React.useState([]);
+  React.useEffect(() => {
+    let cmdClickedTime = 0;
+    const onUndo = e => {
+      if (e.key === "Meta") {
+        cmdClickedTime = Date.now();
+      }
+
+      if (e.key === "z") {
+        if (Date.now() - cmdClickedTime < 1000) {
+        }
+      }
+    };
+    document.addEventListener("keydown", onUndo);
+    return () => document.removeEventListener("keydown", onUndo);
+    // eslint-display-next-line
+  }, []);
   return (
     <div className="App">
       <DNSContainer width={500} height={700}>
         <DNSImage
-          data={imageData}
+          data={data}
           onChangeEnd={newImageData => {
-            console.log("new image data");
+            if (history.length > 10) {
+              history.splice(0, 1);
+            }
+            history.push({ action: "data-change", data: newImageData });
+            setHistory([...history]);
           }}
           onRemove={data => {
             console.log("Remove", data);
