248f8a61cff6c1e99f52a1f5ac1fb4441feaafa8
lgdelacruz <bboyjacks@gmail.com>
2019-11-26 06:35:34 -0800

    State not updating

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { imageData } from "./dnscontainer/data";
 function App() {
   const [data, setImageData] = React.useState(imageData);
   const [history, setHistory] = React.useState([]);
+  const [historyPos, setHistoryPos] = React.useState(0);
   React.useEffect(() => {
     let cmdClickedTime = 0;
     const onUndo = e => {
@@ -15,23 +16,29 @@ function App() {
 
       if (e.key === "z") {
         if (Date.now() - cmdClickedTime < 1000) {
+          console.log("Undo", historyPos);
+          if (historyPos > 0) {
+            console.log("Found something");
+            setImageData({ ...history[historyPos - 1].data });
+          }
         }
       }
     };
     document.addEventListener("keydown", onUndo);
     return () => document.removeEventListener("keydown", onUndo);
     // eslint-display-next-line
-  }, []);
+  }, [historyPos]);
   return (
     <div className="App">
       <DNSContainer width={500} height={700}>
         <DNSImage
           data={data}
-          onChangeEnd={newImageData => {
+          onChangeEnd={(e, newImageData) => {
             if (history.length > 10) {
               history.splice(0, 1);
             }
             history.push({ action: "data-change", data: newImageData });
+            setHistoryPos(historyPos + 1);
             setHistory([...history]);
           }}
           onRemove={data => {
